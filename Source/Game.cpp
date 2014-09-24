bb2201736faa3b37d7613a477d932d5fd3bfd36a
Yesenia Huerta <yesenia.huerta10@gmail.com>
2014-09-24 16:29:36 -0700

    Actividad 3

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -6,13 +6,32 @@
 
 CGame::CGame(){
 	estado = ESTADO_INICIANDO;
+	atexit(SDL_Quit);
 }
 
 // Con esta función eliminaremos todos los elementos en pantalla
-void CGame::Finalize(){
+void CGame::Finalize()
+{
+	SDL_Quit();
 }
 
-void CGame::Iniciando(){}
+void CGame::Iniciando()
+{
+	if (SDL_Init(SDL_INIT_VIDEO))
+	{
+		printf("Error %s ", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+
+	screen = SDL_SetVideoMode(640, 480, 24, SDL_SWSURFACE);
+	if (screen == NULL)
+	{
+		printf("Error %s ", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+
+	SDL_WM_SetCaption("Mi primer Juego", NULL);
+}
 bool CGame::Start()
 {
 	// Esta variable nos ayudara a controlar la salida del juego...
@@ -52,6 +71,7 @@ bool CGame::Start()
 		case Estado::ESTADO_FINALIZANDO:
 			break;
 		};
+		SDL_Flip(screen);
     }
 	return true;
 }
